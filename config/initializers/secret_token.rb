2a5ccef1d154b2e406f2b7fee748101fead0c107
hash4di <hash4di@doubledrones.com>
2011-07-08 10:12:26 +0200

    create fresh rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecutune::Application.config.secret_token = '1995f384f91d1444331f3a797592eb9604f12b5190d0df3a25a5d81d5f15cdbca4e94de439347c16e1b85ddf89c46cdbef8245fe9223eb85d4500fa99caf9a5f'
